6755215fa814fee804d234126bc6642624bfdc00
Arthur Brinkmann <arthur.brinkmann@rgi-corp.com>
2023-11-28 12:29:17 -0600

    Adding rest of files

---
+++ Section14Section9RemasterinProgress/ModulesAndFunctions/datetime_example.py
@@ -0,0 +1,8 @@
+import datetime as dt
+from datetime import datetime
+
+now = datetime.now()
+utc_now = datetime.utcnow()
+
+print(now)
+print(utc_now)
---
+++ Section14Section9RemasterinProgress/ModulesAndFunctions/open_url.py
@@ -0,0 +1,7 @@
+import webbrowser
+
+# webbrowser.open('https://docs.python.org/3.10/library/webbrowser')
+
+# chrome = webbrowser.get(using='google-chrome')
+chrome = webbrowser.get('C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s')
+chrome.open('https://learnprogramming.academy/courses')
